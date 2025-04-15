68a3c847f51d4418ecaf4c485cfa4e7f6e4071ee
Lucas Kells <kellucd@dunwoody.edu>
2025-04-15 10:54:55 -0500

    fixes

--- backend.tf
+++ backend.tf
@@ -1,10 +1,20 @@
+#us-east-1
 terraform {
-  
   backend "s3" {
-    bucket   = "${var.backend-s3-bucket-name}"
-    key      = "${var.backend-s3-bucket-key}"
-    region   = "${var.infra-region}"
-    dynamodb_table = "${var.backend-dynamodb-table}"
-    encrypt = true
+    bucket         = "lk-tform-bucket"
+    key            = "terraform.tfstate"
+    region         = ["us-east-1", "us-east-2"]
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
   }
 }
+
+
+# backend "s3" {
+#  bucket   = "${var.backend-s3-bucket-name}"
+#  key      = "${var.backend-s3-bucket-key}"
+#  region   = "${var.infra-region}"
+#  dynamodb_table = "${var.backend-dynamodb-table}"
+#  encrypt = true
+#}
+#}
