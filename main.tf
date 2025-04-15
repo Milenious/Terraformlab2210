6c87b9830843bc3cfa3276f7678b6f708c855749
Lucas Kells <kellucd@dunwoody.edu>
2025-04-15 12:19:57 -0500

    corrected a 1D10T error

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_instance" "lk_instance_web" {
   instance_type               = "${var.instance_type}"
   subnet_id                   = element(aws_subnet.public_subnet[*].id, count.index)
   associate_public_ip_address = true
-  vpc_security_group_ids      = element(aws_subnet.public_subnet[*].id, count.index)
+  vpc_security_group_ids      = ["${aws_security_group.lk-web-sg.id}"]
   
   tags = {
     name = "lk_instance_web_${count.index}"
