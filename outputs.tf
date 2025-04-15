a8c82e8d34cd1cb58661a398da2875ad7d1a3a79
Lucas Kells <kellucd@dunwoody.edu>
2025-04-15 12:15:13 -0500

    updated outputs to match the current syntax of the updated code

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,16 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = aws_vpc.vpc.id
+  value       = "${aws_vpc.main_infra_vpc.id}"
 }
 
 output "subnet_ids" {
   description = "List of IDs of public subnets"
-  value       = aws_subnet.public_subnet[*].id
+  value       = "${aws_subnet.public_subnet[*].id}"
+}
+
+output "instance_public_ips" {
+  description = "List of IDs of public subnets"
+  value = "${aws_instance.lk_instance_web[*].public_ip}"
 }
 
   
